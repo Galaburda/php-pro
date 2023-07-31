89fe618785e7044e3d063a723a7eb59c20012c91
vlad <galaburda_vlad@i.ua>
2023-07-31 17:24:21 +0300

    lesson11n

--- app/Http/Requests/User/UserLoginRequest.php
+++ app/Http/Requests/User/UserLoginRequest.php
@@ -14,16 +14,7 @@ class UserLoginRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //TODO:для емейлу рекомендую додавати додаткові умови.
-            // якщо користуватися стандартними які є в валідаторі
-            // https://laravel.com/docs/10.x/validation#rule-email то це email:rfc,dns
             'email' => ['required', 'string', 'email', 'max:255'],
-            //TODO:для пароля рекомендую додати більш складніші умови
-            // Password::min(8)
-            // ->mixedCase() # Потрібна хоча б одна велика і одна мала літери
-            // ->numbers() # Потрібна хоча б одна цифра
-            // ->symbols() # Потрібний хоча б один символ
-            // це як приклад...
             'password' => ['required', 'min:6'],
         ];
     }
