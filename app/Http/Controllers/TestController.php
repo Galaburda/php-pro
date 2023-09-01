da8df09e1920eac3ad3fa480cd492568eefda607
vlad <galaburda_vlad@i.ua>
2023-09-01 13:30:54 +0300

    test controller request

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+class TestController extends Controller
+{
+    public function index()
+    {
+//        echo 'test index';
+//        echo PHP_EOL;
+//        echo auth()->id();
+//        echo PHP_EOL;
+//        var_dump(Route::currentRouteName());
+    }
+}
